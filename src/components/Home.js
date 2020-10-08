4378e2dffa2adf62430b873e47c20572f1647767
hansol <ekim569@gmail.com>
2020-10-08 17:23:44 +0900

    second commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,9 +5,18 @@ export default function Home() {
   return (
     <Container>
       <Row>
-        <Col>1 of 3</Col>
-        <Col xs={6}>2 of 3 (wider)</Col>
-        <Col>3 of 3</Col>
+        <Col
+          style={{
+            margintop: 139,
+            fontSize: 50,
+            fontFamily: "Malgun Gothic",
+            fontWeight: "bold",
+          }}
+        >
+          우리는 당연한 일을 해드리는거죠
+          <br />
+          당신의 불편한 일들을
+        </Col>
       </Row>
       <button href="/src/pages/onepage.js"> 서비스소개</button>
       <br />
